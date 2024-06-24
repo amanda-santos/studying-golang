027d8904dfe4bbd3d9e4b456c4cac63d351bc38a
amanda-santos <amandasf0200@gmail.com>
2024-06-24 18:15:43 -0300

    feat: add Go channels example

---
+++ channels/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		// the link string we are passing here is the value returned by the channel in "<-c"
+		// same as `go checkLink(<-c, c)`
+		// go checkLink(l, c)
+
+		// this is a function literal, same as a anonymous function
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
